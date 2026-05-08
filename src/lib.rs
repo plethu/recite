a719a6b1de027dedbee01c97a414b0bb1a0cf37a
Allie <allie@plethu.co.uk>
2026-05-08 03:30:29 +0100

    Add placeholder library crate

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+//! wip deterministic dialog runtime & tooling for ECS-oriented game development
+//!
+//! Recite is in early development. Public APIs are not available yet.
+
+#![forbid(unsafe_code)]
