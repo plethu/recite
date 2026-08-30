3a77d8b81de4b9219ae888f50f0aed815b9505cc
Allie <allie@plethu.co.uk>
2026-08-30 16:13:54 +0100

    [REC-187] fix(compiler): export scoped authoring plans

--- crates/recite-compiler/src/authoring/edit/mod.rs
+++ crates/recite-compiler/src/authoring/edit/mod.rs
@@ -5,6 +5,7 @@ mod operation;
 mod plan;
 mod plan_rename;
 mod plan_stable_ids;
+mod plan_stable_ids_scope;
 mod plan_stub;
 mod precondition;
 mod range;
@@ -13,9 +14,9 @@ mod types;
 mod validate;
 
 pub use plan_rename::plan_rename_block;
-pub use plan_stable_ids::{
-    plan_insert_missing_id, plan_insert_missing_ids, plan_insert_missing_ids_for_document,
-    plan_insert_missing_ids_in_range,
+pub use plan_stable_ids::{plan_insert_missing_id, plan_insert_missing_ids};
+pub use plan_stable_ids_scope::{
+    plan_insert_missing_ids_for_document, plan_insert_missing_ids_in_range,
 };
 pub use plan_stub::{plan_create_block_stub, plan_create_block_stub_in_range};
 pub use types::{
